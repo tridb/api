3b6b136f19387027c12cfd0f162e4a7c32105f47
Pedro Santos <donbonifacio@gmail.com>
2014-05-06 09:47:47 +0100

    Add root api endpoint

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,14 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = Action { request =>
+    Ok(Json.obj(
+      "status" -> "OK"
+    ))
   }
 
 }
